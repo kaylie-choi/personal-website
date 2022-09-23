8e2af8463dba2a1c4d04bdcc8680ad84c9458c31
kaylie-choi <k33choi@uwaterloo.ca>
2022-09-22 17:51:41 -0700

    add syde542 portfolio + update  project card link styling

--- src/components/projects/projects-container.js
+++ src/components/projects/projects-container.js
@@ -4,11 +4,13 @@ import ProjectCard from './project-card'
 import pixelPaint from "../../assets/projects-pixelPaint.png"
 import shoppableGallery from "../../assets/projects-shoppableGallery.png"
 import productRecos from "../../assets/projects-productRecos.png"
+import syde542 from "../../assets/projects-syde542.png"
 import {
   FaLink, 
   FaLaptopCode,
   FaPalette,
-  FaShareSquare
+  FaShareSquare,
+  FaArrowRight
 } from "react-icons/fa";
 
 
@@ -16,9 +18,26 @@ const ProjectsWrapper = styled.div`
     padding: 1rem 0 6rem;
 `
 
-export default function Projects()  {
+export default function Projects() {
   return (
     <ProjectsWrapper>
+
+      <ProjectCard 
+        img={syde542}
+        alt="design portfolio img"
+        title="Design Portfolio"
+        subtitle="Interface Design Final Project"
+        desc="Proposing, prototyping and evaluating 2 different interfaces, focusing on 
+        the importance of design goals, user requirements/testing, and iteration."
+        firstLink="https://drive.google.com/file/d/1GPQevMvKslvRS94Q1lKg9pJOFUFx8tBK/view?usp=sharing"
+        firstLinkText="Design Portfolio "
+        firstIcon={<FaArrowRight title='view portfolio'/>}
+        secondLink="https://drive.google.com/file/d/1hqB0x9LNb57wwirxrgTXDgNBoRg1PUh5/view?usp=sharing"
+        secondLinkText="Portfolio Appendix "
+        secondIcon={<FaArrowRight title='view appendix'/> }
+      >
+      </ProjectCard>
+
       <ProjectCard 
         img={shoppableGallery}
         alt="ptt badges img"
@@ -26,8 +45,8 @@ export default function Projects()  {
         subtitle="Pixlee TurnTo"
         desc="Developing and releasing a new dynamic product badge and discount pricing features within photo galleries."
         firstLink="https://kayliechoi.notion.site/Shoppable-Social-UGC-Widgets-2afc4b5ba59f4aa7bdb2601913c78986"
-        firstIcon={<FaLink title='view case study'/>}
-        firstLinkText="Case Study"
+        firstLinkText="Case Study "
+        firstIcon={<FaArrowRight title='view case study'/>}
       >
       </ProjectCard>
 
@@ -39,8 +58,8 @@ export default function Projects()  {
         desc="Ideating and developing a rapid proof of concept to suggest recommended alternatives when products featured 
         in user generated content are out of stock."
         firstLink="https://kayliechoi.notion.site/Out-of-Stock-Product-Recommendations-9bd04a523444436eb74f0107e349bdce"
-        firstIcon={<FaLink title='view case study'/>}
-        firstLinkText="Case Study"
+        firstLinkText="Case Study "
+        firstIcon={<FaArrowRight title='view case study'/>}
       >
       </ProjectCard>
 
@@ -66,11 +85,11 @@ export default function Projects()  {
         subtitle="Personal Project"
         desc="A pixel art editor React app inspired by the retro design of Windows95 MS Paint."
         firstLink="https://github.com/kaylie-choi/pixel-paint"
-        firstIcon={<FaLaptopCode title='view source code'/> }
-        firstLinkText="View Source"
+        firstLinkText="View Source "
+        firstIcon={<FaArrowRight title='view source code'/> }
         secondLink="https://kaylie-choi.github.io/pixel-paint/"
-        secondIcon={<FaShareSquare title='view live demo'/>}
-        secondLinkText="Live Demo"
+        secondLinkText="Live Demo "
+        secondIcon={<FaArrowRight title='view live demo'/>}
       >
       </ProjectCard>
     </ProjectsWrapper>
